298d6587b971f5a2a5f9e07e77d910152339df3a
Dpaace <dipeshnepali767@gmail.com>
2022-05-15 15:12:03 +0545

    Updates

--- BookStore/BookStore/settings.py
+++ BookStore/BookStore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'register'
+    'register',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BookStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- BookStore/register/urls.py
+++ BookStore/register/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path,include
+from django.urls import path, include
 from register import views
 
 
-urlpatterns =[
-    path("",views.Register),
-     path("home",views.home),
-     path("loginn",views.loginn),
-     path("profile",views.profile)
-   
+urlpatterns = [
+    path("", views.home, name='home'),
+    path("register", views.Register, name='register'),
+    path("loginn", views.loginn, name='loginn'),
+    path("profile", views.profile, name='profile')
+
 ]
