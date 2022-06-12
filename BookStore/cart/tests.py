6b947026949508a95644b1f7c5400cc2fdb10470
Dpaace <dipeshnepali767@gmail.com>
2022-06-12 16:18:47 +0545

    Sprint5 Tests

--- BookStore/cart/tests.py
+++ BookStore/cart/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse,resolve
+from django.test import Client, SimpleTestCase, TestCase
+from cart.views import cart, updateItem, checkout, processOrder, remove_order, delete_order
 
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+    def test_case_cart_url(self):
+        url=reverse("cart")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,cart)
+
+    def test_case_update_item_url(self):
+        url=reverse("update_item")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,updateItem)
+
+    def test_case_checkout_url(self):
+        url=reverse("checkout")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,checkout)
+
+    def test_case_process_order_url(self):
+        url=reverse("process_order")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,processOrder)
+
+    def test_case_remove_order_url(self):
+        url=reverse("remove_order", args=[1])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,remove_order)
+
+    def test_case_delete_order_url(self):
+        url=reverse("delete_order", args=[1])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,delete_order)
+
