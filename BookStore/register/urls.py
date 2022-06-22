740f5c65706f34951bcf397c5d8adb2bef0faf74
Marinagansi <78782374+Marinagansi@users.noreply.github.com>
2022-06-22 17:21:33 +0545

    order

--- BookStore/register/urls.py
+++ BookStore/register/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
          name='password_reset'),
 
     path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(git
+         auth_views.PasswordResetDoneView.as_view(
              template_name='password/password_reset_done.html'),
          name='password_reset_done'),
 
